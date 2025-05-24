7aec1bd71b5ef2a746a73399ab5dfd27b13764d1
yurim <y1551463@gmail.com>
2025-05-24 14:47:57 +0900

    Init

---
+++ src/test/java/io/github/Yuurim98/digi_capsule/DigiCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Yuurim98.digi_capsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiCapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
