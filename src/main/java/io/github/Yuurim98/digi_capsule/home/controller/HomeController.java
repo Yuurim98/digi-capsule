9af9df612c137ae4a85b7b05cbda05e31ba974fc
yurim <y1551463@gmail.com>
2025-05-25 21:19:00 +0900

    Feat: index 추가
    메인 홈 화면 생성 및 컨트롤러 생성
    
    Closes #2

---
+++ src/main/java/io/github/Yuurim98/digi_capsule/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package io.github.Yuurim98.digi_capsule.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
